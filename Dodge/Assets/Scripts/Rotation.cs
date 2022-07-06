514cae666aad54062be933729abe4a0a29d055f2
puter <puter2712goo@gmail.com>
2022-07-07 01:18:11 +0900

    Dodge Final Commit

---
+++ Dodge/Assets/Scripts/Rotation.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+  public float rotationSpeed = 60f;
+
+  void Update() {
+    transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+  }
+}
